f75fe59d2de54f66477baf71b8505bfd03f33fe0
ikechukwu edwin <eo@hellofresh.com>
2018-08-20 09:54:20 +0200

    Attempt to setup the public folder for resources

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let exphbs = require('express-handlebars');
+let path = require('path');
 
 let app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('home');
